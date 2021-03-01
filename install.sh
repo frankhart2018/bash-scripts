eb2ca13578065f131d3766cf7edbc10b2be28152
frankhart <siddharthadhar.soumen@srmuniv.edu.in>
2021-03-01 21:10:13 +0530

    Initial commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "Installing git push"
+sudo cp push /usr/local/bin/
+sudo chmod +x /usr/local/bin/push
+
+echo "Installing git fetch pull"
+sudo cp git-fpull /usr/local/bin/
+sudo chmod +x /usr/local/bin/git-fpull
