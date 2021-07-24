5ca4fa2edf3b7aab96503e7ffa9d9d0a010e0e88
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-07-24 12:40:11 +0530

    Add tarball with progress script

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ sudo chmod +x /usr/local/bin/push
 echo "Installing git fetch pull"
 sudo cp git-fpull /usr/local/bin/
 sudo chmod +x /usr/local/bin/git-fpull
+
+echo "Installing ptar"
+sudo cp ptar /usr/local/bin/
+sudo chmod +x /usr/local/bin/ptar
