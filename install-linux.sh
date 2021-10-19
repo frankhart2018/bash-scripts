a58d7a3bd244d4a5bc3bdac3475eb073b20ebeb3
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-10-19 01:44:32 -0400

    Add git-init

--- install-linux.sh
+++ install-linux.sh
@@ -1,24 +1,28 @@
 sudo echo 'Installing in linux...'
 
+echo 'Installing loc'
+sudo cp ./common/loc /usr/bin
+sudo chmod +x /usr/bin/loc
+
+echo 'Installing git-init'
+sudo cp ./common/git-init /usr/bin
+sudo chmod +x /usr/bin/git-init
+
 echo 'Installing git-fpull'
 sudo cp ./common/git-fpull /usr/bin
 sudo chmod +x /usr/bin/git-fpull
+
+echo 'Installing push'
+sudo cp ./common/push /usr/bin
+sudo chmod +x /usr/bin/push
 
 echo 'Installing gccc'
 sudo cp ./common/gccc /usr/bin
 sudo chmod +x /usr/bin/gccc
 
-echo 'Installing loc'
-sudo cp ./common/loc /usr/bin
-sudo chmod +x /usr/bin/loc
-
 echo 'Installing gppc'
 sudo cp ./common/gppc /usr/bin
 sudo chmod +x /usr/bin/gppc
-
-echo 'Installing push'
-sudo cp ./common/push /usr/bin
-sudo chmod +x /usr/bin/push
 
 echo 'Installing ptar'
 sudo cp ./linux/ptar /usr/bin
--- install-macos.sh
+++ install-macos.sh
@@ -1,24 +1,28 @@
 sudo echo 'Installing in macOS...'
 
+echo 'Installing loc'
+sudo cp ./common/loc /usr/local/bin
+sudo chmod +x /usr/local/bin/loc
+
+echo 'Installing git-init'
+sudo cp ./common/git-init /usr/local/bin
+sudo chmod +x /usr/local/bin/git-init
+
 echo 'Installing git-fpull'
 sudo cp ./common/git-fpull /usr/local/bin
 sudo chmod +x /usr/local/bin/git-fpull
+
+echo 'Installing push'
+sudo cp ./common/push /usr/local/bin
+sudo chmod +x /usr/local/bin/push
 
 echo 'Installing gccc'
 sudo cp ./common/gccc /usr/local/bin
 sudo chmod +x /usr/local/bin/gccc
 
-echo 'Installing loc'
-sudo cp ./common/loc /usr/local/bin
-sudo chmod +x /usr/local/bin/loc
-
 echo 'Installing gppc'
 sudo cp ./common/gppc /usr/local/bin
 sudo chmod +x /usr/local/bin/gppc
-
-echo 'Installing push'
-sudo cp ./common/push /usr/local/bin
-sudo chmod +x /usr/local/bin/push
 
 echo 'Installing ptar'
 sudo cp ./macos/ptar /usr/local/bin
